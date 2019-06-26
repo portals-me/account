ecfdaa2bd96e0be1ebbf03a9c60bc55abd0c1e3f
myuon <ioi.joi.koi.loi@gmail.com>
2019-06-27 00:29:27 +0900

    refactor: format

---
+++ functions/signup/main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/pkg/errors"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/guregu/dynamo"
+
+	"github.com/portals-me/account/functions/signin/auth"
+	"github.com/portals-me/account/lib/jwt"
+	"github.com/portals-me/account/lib/user"
+)
+
+var authTableName = os.Getenv("authTable")
+var jwtPrivateKey = os.Getenv("jwtPrivate")
+var twitterClientKey = os.Getenv("twitterClientKey")
+var twitterClientSecret = os.Getenv("twitterClientSecret")
+
+type Input struct {
+	AuthType string      `json:"auth_type"`
+	Data     interface{} `json:"data"`
+}
+
+// Crate an Auth method from requestBody
+// This function should an instance constructing function
+func createAuthMethod(body string) (auth.AuthMethod, error) {
+	var input Input
+	if err := json.Unmarshal([]byte(body), &input); err != nil {
+		return nil, errors.Wrap(err, "Unmarshal failed")
+	}
+
+	if input.AuthType == "password" {
+		var password auth.Password
+
+		data, _ := json.Marshal(input.Data)
+		if err := json.Unmarshal([]byte(data), &password); err != nil {
+			return nil, errors.Wrap(err, "Unmarshal password failed")
+		}
+
+		return password, nil
+	} else if input.AuthType == "twitter" {
+		var credentials auth.TwitterCredentials
+
+		data, _ := json.Marshal(input.Data)
+		if err := json.Unmarshal([]byte(data), &credentials); err != nil {
+			return nil, errors.Wrap(err, "Unmarshal twitter failed")
+		}
+
+		return auth.Twitter{
+			TwitterCredentials: credentials,
+			ClientKey:          twitterClientKey,
+			ClientSecret:       twitterClientSecret,
+		}, nil
+	}
+
+	return nil, errors.New("Unsupported auth_type: " + input.AuthType)
+}
+
+func tryDecodeBase64(s string) string {
+	decoded, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return s
+	}
+
+	return string(decoded)
+}
+
+func createJwt(userInfo user.UserInfo) (string, error) {
+	payload, err := json.Marshal(userInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	signer := jwt.ES256Signer{
+		Key: jwtPrivateKey,
+	}
+	token, err := signer.Sign(payload)
+	if err != nil {
+		return "", errors.Wrap(err, "sign failed")
+	}
+
+	return string(token), nil
+}
+
+/*	POST /authenticate
+
+	expects Input
+	returns String (jwt)
+*/
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// try base64 decoding
+	body := tryDecodeBase64(request.Body)
+	fmt.Println(body)
+
+	method, err := createAuthMethod(body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
+	}
+
+	sess := session.Must(session.NewSession())
+
+	db := dynamo.NewFromIface(dynamodb.New(sess))
+	authTable := db.Table(authTableName)
+
+	// Get Idp ID
+	idpID, err := method.ObtainUserID(authTable)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
+	}
+
+	// Get UserInfo from "detail" part
+	var record user.UserInfoDDB
+	if err := authTable.
+		Get("id", idpID).
+		Range("sort", dynamo.Equal, "detail").
+		One(&record); err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 404}, nil
+	}
+
+	// Create JWT
+	jwt, err := createJwt(record.UserInfo)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body: jwt,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin": "*",
+		},
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
--- index.ts
+++ index.ts
@@ -1,111 +1,121 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import { createLambdaMethod, createCORSResource } from "./infrastructure/apigateway";
+import {
+  createLambdaMethod,
+  createCORSResource
+} from "./infrastructure/apigateway";
 import { createLambdaFunction } from "./infrastructure/lambda";
 
 const config = {
   service: new pulumi.Config().name,
-  stage: pulumi.getStack(),
+  stage: pulumi.getStack()
 };
 
 const parameter = {
-  jwtPrivate: aws.ssm.getParameter({
-    name: `${config.service}-${config.stage}-jwt-private`,
-    withDecryption: true,
-  }).then(result => result.value),
+  jwtPrivate: aws.ssm
+    .getParameter({
+      name: `${config.service}-${config.stage}-jwt-private`,
+      withDecryption: true
+    })
+    .then(result => result.value),
   twitter: {
-    client: aws.ssm.getParameter({
-      name: `${config.service}-twitter-apiKey`,
-      withDecryption: true,
-    }).then(result => result.value),
-    secret: aws.ssm.getParameter({
-      name: `${config.service}-twitter-apiKey-secret`,
-      withDecryption: true,
-    }).then(result => result.value),
+    client: aws.ssm
+      .getParameter({
+        name: `${config.service}-twitter-apiKey`,
+        withDecryption: true
+      })
+      .then(result => result.value),
+    secret: aws.ssm
+      .getParameter({
+        name: `${config.service}-twitter-apiKey-secret`,
+        withDecryption: true
+      })
+      .then(result => result.value)
   }
 };
 
-const lambdaRole = new aws.iam.Role('auth-lambda-role', {
-  assumeRolePolicy: aws.iam.getPolicyDocument({
-    statements: [{
-      actions: ['sts:AssumeRole'],
-      principals: [{
-        identifiers: ['lambda.amazonaws.com'],
-        type: 'Service'
-      }],
-    }]
-  }).then(result => result.json),
-})
-new aws.iam.RolePolicyAttachment('auth-lambda-role-lambdafull', {
+const lambdaRole = new aws.iam.Role("auth-lambda-role", {
+  assumeRolePolicy: aws.iam
+    .getPolicyDocument({
+      statements: [
+        {
+          actions: ["sts:AssumeRole"],
+          principals: [
+            {
+              identifiers: ["lambda.amazonaws.com"],
+              type: "Service"
+            }
+          ]
+        }
+      ]
+    })
+    .then(result => result.json)
+});
+new aws.iam.RolePolicyAttachment("auth-lambda-role-lambdafull", {
   role: lambdaRole,
-  policyArn: aws.iam.AWSLambdaFullAccess,
+  policyArn: aws.iam.AWSLambdaFullAccess
 });
 
-const accountTable = new aws.dynamodb.Table('account-table', {
+const accountTable = new aws.dynamodb.Table("account-table", {
   attributes: [
     {
-      name: 'id',
-      type: 'S',
+      name: "id",
+      type: "S"
     },
     {
-      name: 'sort',
-      type: 'S',
+      name: "sort",
+      type: "S"
     }
   ],
-  hashKey: 'id',
-  rangeKey: 'sort',
-  billingMode: 'PAY_PER_REQUEST',
-  globalSecondaryIndexes: [{
-    name: 'auth',
-    hashKey: 'sort',
-    rangeKey: 'id',
-    projectionType: 'ALL',
-  }],
+  hashKey: "id",
+  rangeKey: "sort",
+  billingMode: "PAY_PER_REQUEST",
+  globalSecondaryIndexes: [
+    {
+      name: "auth",
+      hashKey: "sort",
+      rangeKey: "id",
+      projectionType: "ALL"
+    }
+  ],
   streamEnabled: true,
-  streamViewType: 'NEW_IMAGE'
+  streamViewType: "NEW_IMAGE"
 });
 
-const accountTableEventTopic = new aws.sns.Topic('account-table-event-topic', {
+const accountTableEventTopic = new aws.sns.Topic("account-table-event-topic", {
   name: `${config.service}-${config.stage}-account-table-event-topic`
 });
 
-const accountTableEventSubscription = createLambdaFunction('account-table-event-subscription', {
-  filepath: 'account-table-subscription',
-  role: lambdaRole,
-  handlerName: `${config.service}-${config.stage}-account-table-event-subscription`,
-  lambdaOptions: {
-    environment: {
-      variables: {
-        timestamp: new Date().toLocaleString(),
-        accountTableSubscriptionTopicArn: accountTableEventTopic.arn,
+const accountTableEventSubscription = createLambdaFunction(
+  "account-table-event-subscription",
+  {
+    filepath: "account-table-subscription",
+    role: lambdaRole,
+    handlerName: `${config.service}-${
+      config.stage
+    }-account-table-event-subscription`,
+    lambdaOptions: {
+      environment: {
+        variables: {
+          timestamp: new Date().toLocaleString(),
+          accountTableSubscriptionTopicArn: accountTableEventTopic.arn
+        }
       }
-    },
+    }
   }
-});
+);
 
-const accountTableSubscription = new aws.dynamodb.TableEventSubscription('account-table-subscription', accountTable, accountTableEventSubscription, {
-  startingPosition: 'TRIM_HORIZON'
-});
-
-const signinLambda = createLambdaFunction('handler-signin', {
-  filepath: 'signin',
-  role: lambdaRole,
-  handlerName: `${config.service}-${config.stage}-signin`,
-  lambdaOptions: {
-    environment: {
-      variables: {
-        timestamp: new Date().toLocaleString(),
-        authTable: accountTable.name,
-        jwtPrivate: parameter.jwtPrivate,
-        twitterClientKey: parameter.twitter.client,
-        twitterClientSecret: parameter.twitter.secret,
-      }
-    },
+const accountTableSubscription = new aws.dynamodb.TableEventSubscription(
+  "account-table-subscription",
+  accountTable,
+  accountTableEventSubscription,
+  {
+    startingPosition: "TRIM_HORIZON"
   }
-});
+);
 
-const twitterLambda = createLambdaFunction('handler-twitter', {
-  filepath: 'twitter',
+const twitterLambda = createLambdaFunction("handler-twitter", {
+  filepath: "twitter",
   role: lambdaRole,
   handlerName: `${config.service}-${config.stage}-twitter`,
   lambdaOptions: {
@@ -113,79 +123,96 @@ const twitterLambda = createLambdaFunction('handler-twitter', {
       variables: {
         timestamp: new Date().toLocaleString(),
         clientKey: parameter.twitter.client,
-        clientSecret: parameter.twitter.secret,
+        clientSecret: parameter.twitter.secret
       }
-    },
+    }
   }
 });
 
-const accountAPI = new aws.apigateway.RestApi('account-api', {
+const accountAPI = new aws.apigateway.RestApi("account-api", {
   name: `${config.service}-${config.stage}`
 });
 
-const signinResource = createCORSResource('signin', {
-  parentId: accountAPI.rootResourceId,
-  pathPart: 'signin',
-  restApi: accountAPI,
-});
-
-const signinLambdaIntegration = createLambdaMethod('signin', {
-  authorization: 'NONE',
-  httpMethod: 'POST',
-  resource: signinResource,
+const signinLambdaIntegration = createLambdaMethod("signin", {
+  authorization: "NONE",
+  httpMethod: "POST",
+  resource: createCORSResource("signin", {
+    parentId: accountAPI.rootResourceId,
+    pathPart: "signin",
+    restApi: accountAPI
+  }),
   restApi: accountAPI,
   integration: {
-    type: 'AWS_PROXY',
+    type: "AWS_PROXY"
   },
-  handler: signinLambda,
+  handler: createLambdaFunction("handler-signin", {
+    filepath: "signin",
+    role: lambdaRole,
+    handlerName: `${config.service}-${config.stage}-signin`,
+    lambdaOptions: {
+      environment: {
+        variables: {
+          timestamp: new Date().toLocaleString(),
+          authTable: accountTable.name,
+          jwtPrivate: parameter.jwtPrivate,
+          twitterClientKey: parameter.twitter.client,
+          twitterClientSecret: parameter.twitter.secret
+        }
+      }
+    }
+  })
 });
 
-const twitterResource = createCORSResource('twitter', {
+const twitterResource = createCORSResource("twitter", {
   parentId: accountAPI.rootResourceId,
-  pathPart: 'twitter',
-  restApi: accountAPI,
+  pathPart: "twitter",
+  restApi: accountAPI
 });
 
-const twitterPostIntegration = createLambdaMethod('twitter-post', {
-  authorization: 'NONE',
-  httpMethod: 'POST',
+const twitterPostIntegration = createLambdaMethod("twitter-post", {
+  authorization: "NONE",
+  httpMethod: "POST",
   resource: twitterResource,
   restApi: accountAPI,
   integration: {
-    type: 'AWS_PROXY',
+    type: "AWS_PROXY"
   },
-  handler: twitterLambda,
+  handler: twitterLambda
 });
 
-const twitterGetIntegration = createLambdaMethod('twitter-get', {
-  authorization: 'NONE',
-  httpMethod: 'GET',
+const twitterGetIntegration = createLambdaMethod("twitter-get", {
+  authorization: "NONE",
+  httpMethod: "GET",
   resource: twitterResource,
   restApi: accountAPI,
   integration: {
-    type: 'AWS_PROXY',
+    type: "AWS_PROXY"
   },
   handler: twitterLambda,
   method: {
     requestParameters: {
-      'method.request.querystring.oauth_token': true,
-      'method.request.querystring.oauth_verifier': true,
+      "method.request.querystring.oauth_token": true,
+      "method.request.querystring.oauth_verifier": true
     }
-  },
+  }
 });
 
 const accountAPIDeployment = new aws.apigateway.Deployment(
-  'account-api-deployment',
+  "account-api-deployment",
   {
     restApi: accountAPI,
     stageName: config.stage,
-    stageDescription: new Date().toLocaleString(),
+    stageDescription: new Date().toLocaleString()
   },
   {
-    dependsOn: [signinLambdaIntegration, twitterPostIntegration, twitterGetIntegration]
+    dependsOn: [
+      signinLambdaIntegration,
+      twitterPostIntegration,
+      twitterGetIntegration
+    ]
   }
 );
 
 export const output = {
-  endpoint: accountAPIDeployment.invokeUrl,
+  endpoint: accountAPIDeployment.invokeUrl
 };
